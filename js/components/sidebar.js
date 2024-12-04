6bf0baa4ad054f6bb36f2fedd7f4fa562a35f7f6
mie <nguyenthihuyenmy239@gmail.com>
2024-12-04 14:02:03 +0700

    Need to fix note later

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -17,3 +17,32 @@ function loadComponent(selector, filePath) {
 document.addEventListener('DOMContentLoaded', () => {
     loadComponent('#sidebar-placeholder', '/pages/components/sidebar.html');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const noteLink = document.getElementById("note-link");
+    const noteModal = document.querySelector(".note-modal");
+    const noteContainer = document.querySelector(".note-container");
+    const closeIcon = document.querySelector(".close-icon");
+    const overlay = document.querySelector(".overlay");
+  
+    // Mở modal khi click vào "Note"
+    noteLink.addEventListener("click", () => {
+      noteModal.style.display = "flex"; // Hiển thị modal
+      noteContainer.style.display = "flex"; // Hiển thị note-container
+      overlay.style.display = "block"; // Hiển thị overlay
+    });
+  
+    // Đóng modal khi click vào biểu tượng đóng (x)
+    closeIcon.addEventListener("click", () => {
+      noteModal.style.display = "none";  // Ẩn modal
+      overlay.style.display = "none";  // Ẩn overlay
+      noteContainer.style.display = "none";
+    });
+  
+    // Đóng modal khi click vào overlay
+    overlay.addEventListener("click", () => {
+      noteModal.style.display = "none";  // Ẩn modal
+      noteContainer.style.display = "none";
+      overlay.style.display = "none";  // Ẩn overlay
+    });
+  });
