a5cddbb5ded093df45f93f48875ca67a819ed5f6
HuyenNguyen <huyen04062003@gmail.com>
2024-12-13 10:51:30 +0700

    sidebar login

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,118 +1,34 @@
-import CONFIG from '../utils/settings.js';
-
-function joinRoom(roomId) {
-    const token = localStorage.getItem('token');
-    
-    if (!token) {
-        alert("Bạn cần đăng nhập để tham gia phòng.");
-        return;
+function changeQuantity(amount) {
+    var quantityInput = document.getElementById('quantity');
+    var newValue = parseInt(quantityInput.value) + amount;
+    if (newValue >= 1) {
+        quantityInput.value = newValue;
     }
-
-    fetch(`http://${CONFIG.BASE_URL}/api/rooms/room/${roomId}/join/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Token ${token}`,
-        },
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.message) {
-            alert(data.message);
-
-            if (data.message === "Bạn đã tham gia phòng thành công.") {
-                window.location.href = `room.html?room_id=${roomId}`; // Ví dụ chuyển hướng đến trang phòng học
-            }
-        }
-    })
-    .catch(error => {
-        console.error('Error joining room:', error);
-        alert('Có lỗi xảy ra khi tham gia phòng.');
-    });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector('.card-container').addEventListener('click', function (event) {
-        if (event.target && event.target.classList.contains('join-button')) {
-            const card = event.target.closest('.card');
-            if (card) {
-                const roomId = card.getAttribute('data-room-id');
-                console.log('Room ID from Join button click:', roomId);
+document.addEventListener("DOMContentLoaded", function() {
+    // Lấy các phần tử HTML cần thao tác
+    const addButton = document.querySelector(".add-btn");
+    const listDetails = document.querySelector(".list-details");
+    const listPrescription = document.querySelector(".list-prescription");
+    const medicineAdd = document.querySelector(".medicine-add");
+    const titleListPre = document.querySelector(".h-prescription");
+    const titleListDetails = document.querySelector(".h-details");
 
-                joinRoom(roomId);
-            }
+    // Lắng nghe sự kiện click vào nút add-btn
+    addButton.addEventListener("click", function() {
+        // Ẩn nút add-btn
+        addButton.style.display = "none";
+        // Hiển thị phần medicine-add
+        medicineAdd.style.display = "grid";
+        // Hiển thị phần list-details
+        listDetails.style.display = "block";
+        titleListDetails.style.display = "block";
+        // Ẩn phần list-prescription
+        if (listPrescription && titleListPre) {
+            listPrescription.style.display = "none";
+            titleListPre.style.display = "none";
         }
-    });
 
-    const username = localStorage.getItem('username');
-    
-    if (username) {
-        document.getElementById('username').innerText = username;
-    }
-
-    const overlay = document.getElementById("overlay");
-    const popup = document.getElementById("popup");
-    const createRoomBtn = document.querySelector(".create-room-btn");
-    const closePopupCancel = document.getElementById("close-popup");
-    const backgroundOptions = document.querySelectorAll("#background-options img");
-    const topicSelect = document.getElementById("room-topic");
-    const selectedTopics = document.getElementById("selected-topics");
-    const closePopupBtn = document.querySelector(".close-popup-btn");
-
-    createRoomBtn.addEventListener("click", () => {
-        overlay.classList.remove("hidden");
-        popup.classList.remove("hidden");
-    });
-
-    closePopupCancel.addEventListener("click", () => {
-        overlay.classList.add("hidden");
-        popup.classList.add("hidden");
-    });
-
-    overlay.addEventListener("click", (e) => {
-        if (e.target === overlay) {
-            overlay.classList.add("hidden");
-            popup.classList.add("hidden");
-        }
-    });
-
-    closePopupBtn.addEventListener("click", () => {
-        overlay.classList.add("hidden");
-        popup.classList.add("hidden");
-    });
-
-    backgroundOptions.forEach((img) => {
-        img.addEventListener("click", () => {
-            backgroundOptions.forEach((i) => i.classList.remove("selected"));
-            img.classList.add("selected");
-            const backgroundInput = document.querySelector("input[name='background']");
-            if (backgroundInput) {
-                backgroundInput.value = img.getAttribute("data-value");
-            }
-        });
-    });
-
-    topicSelect.addEventListener("change", () => {
-        const selectedOptions = Array.from(topicSelect.selectedOptions);
-        selectedTopics.innerHTML = "";
-    
-        selectedOptions.forEach(option => {
-            const topic = option.text;
-    
-            if (![...selectedTopics.children].some((span) => span.textContent === topic)) {
-                const span = document.createElement("span");
-                span.textContent = topic;
-                selectedTopics.appendChild(span);
-    
-                span.addEventListener("click", () => {
-                    selectedTopics.removeChild(span);
-                    
-                    const optionToRemove = Array.from(topicSelect.options).find(opt => opt.text === topic);
-                    if (optionToRemove) {
-                        optionToRemove.selected = false;
-                    }
-                });
-            }
-        });
     });
 });
